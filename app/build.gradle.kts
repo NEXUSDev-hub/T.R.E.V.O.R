0a5a67a914473fed9505726afb71643ea7a28389
NEXUSDev-hub <abhirupgupta523@gmail.com>
2026-09-18 11:49:32 +0530

    fix: add compose material icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,9 +27,11 @@ android {
 
     dependencies {
         implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+
         implementation("androidx.compose.ui:ui")
         implementation("androidx.compose.ui:ui-tooling-preview")
         implementation("androidx.compose.material3:material3")
+        implementation("androidx.compose.material:material-icons-extended")
         implementation("androidx.activity:activity-compose:1.10.0")
 
         debugImplementation("androidx.compose.ui:ui-tooling")
